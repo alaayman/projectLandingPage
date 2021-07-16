cd298d5fed3fbf900e480ea12fa6da4a83fd7e6c
alaayman <alaayman@gmail.com>
2021-07-16 18:59:06 +0200

    finished readme

--- README.md
+++ README.md
@@ -1,11 +1,27 @@
 # Landing Page Project
+
     This is Udacity Landing Page Project for the Web Development Nano Degree
+    it contains Udacity's starter code
+
 ## Table of Contents
 
 - [Description](#Description)
+- [What_I_Did](#WhatIDid)
 
 ## Description
 
+A landing page project that needs a dynamic menu navigation
+
+## WhatIDid
+
 What i did
 
-making the navigation dynamic by itrating through 
+- Build the navigation menu
+- making the navigation dynamic by iterating through sections and creating list and anchor items
+- using document fragment for better performance
+- create links and adding attributes to them using dataset and id
+
+- adding event listener to the ul to scroll programmatically to sections
+  ps: doing that with the link href and using (scroll-behavior: smooth) in the css file is way more efficient but it was a challenge to do it through javascript
+
+- creating an event listener for document scroll and call addActiveClass to iterate through sections and add your-active-class to the one in the viewport
--- js/app.js
+++ js/app.js
@@ -18,40 +18,27 @@
  */
 // declare allSections to hold the sections in the document
 const allSections = document.querySelectorAll("section");
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
-function hideMe() {
-  // To be called from setTimeout function
-}
 
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
-
-// build the nav
+//function to build the nav
 function createNavItems() {
   // declare a document fragment to hold the list items until the for loop ends
   const tempDocFragment = document.createDocumentFragment();
-  //Begin the for loop until the sectin list length
+
   for (let i = 0; i < allSections.length; i++) {
-    const newListElement = document.createElement("li"); // newListItems to hold the created element
-    const newLinkItem = document.createElement("a"); // newLinkItems to hold the link
+    const newListElement = document.createElement("li");
+    const newLinkItem = document.createElement("a");
     newListElement.appendChild(newLinkItem);
-    //set attribuets of link item
-    newLinkItem.classList.toggle("menu__link"); //add menue_link class
+    //set attributes of link item
+    newLinkItem.classList.toggle("menu__link"); //add menu_link class
     newLinkItem.href = "#" + allSections[i].id; //add href destination
-    newLinkItem.textContent = allSections[i].dataset.nav; //pull the section name from the data attributes using dataset
-    //
-    tempDocFragment.appendChild(newListElement); // populating the document fragment
+    newLinkItem.textContent = allSections[i].dataset.nav; //pull section name from the data attributes using dataset
+    // populating the document fragment
+    tempDocFragment.appendChild(newListElement);
   }
   //append the created items to the <ul> navbar element
   const navBar = document.getElementById("navbar__list");
   navBar.appendChild(tempDocFragment);
+  //add eventListener to go to section
   navBar.addEventListener("click", clickedGoToAnchor);
 }
 
@@ -64,16 +51,15 @@ function addActiveClass() {
     } else {
       section.classList.remove("your-active-class");
     }
+    // test placement
     // console.log(section.id + "  " + myPlace.top + " T " + myPlace.bottom + " B " + section.classList);
   }
-  console.log("haha ");
 }
 // Scroll to anchor ID using scrollTO event
-
 function clickedGoToAnchor(event) {
-  // to scroll to sections smoothely after being clicked , need to stop defaultBehavior first
+  // to scroll to sections smoothly after being clicked , need to stop default Behavior first
   event.preventDefault();
-  dest = event.target.getAttribute("href");
+  const dest = event.target.getAttribute("href");
   document.querySelector(dest).scrollIntoView({
     behavior: "smooth",
   });
@@ -81,12 +67,7 @@ function clickedGoToAnchor(event) {
 /**
  * End Main Functions
  * Begin Events
- *
  */
-document.addEventListener("scroll", addActiveClass);
-// Build menu
-
-// Set sections as active
-
 //start the magic and put the items in place
 createNavItems();
+document.addEventListener("scroll", addActiveClass);
